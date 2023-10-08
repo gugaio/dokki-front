017604313ef9cebecb332c3f6428af2c6f457580
Gustavo Barros <gustavo.barros@jurus.com.br>
2023-10-08 11:22:40 -0300

    Home layout improvement

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,11 +1,22 @@
 <template lang="">
-    <div>
-        <h1>Photo Upload App</h1>
-        <input type="file" @change="onFileChange" />
+    <div class="uploader-root">     
+        <form>
+            <div class="photo">
+                <button class="circle-button">
+                    <img src="/camera2.png" alt="Foto" />
+                </button>
+            </div>
+            <h3>ou</h3>
+            <div class="image-label-container">
+                <img src="/upload.jpg" alt="Upload" />
+                <label for="file">Selecionar nota</label>
+                <input id="file" type="file" @change="onFileChange" />
+            </div>
+        </form>
         <div class="preview" v-if="fileData">
             <img :src="fileData" alt="Preview" />
         </div>
-        <button @click="uploadPhoto" v-if="fileData">Upload Photo</button>
+        <button @click="uploadPhoto" v-if="fileData" >Up</button>
   </div>
 </template>
 <script>
@@ -52,6 +63,62 @@ export default {
     
 }
 </script>
-<style lang="">
-    
+<style>
+    .uploader-root {
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 200px;
+    }
+    .photo {
+        margin-left: auto;
+        margin-right: auto;
+        width: 50px;
+    }
+    h3{
+        text-align: center;
+        color: white;
+        margin: 20px;
+    }
+    input[type="file"] {
+        display: none;
+    }
+    .image-label-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;        
+        background-color: white;        
+        border-radius: 10px;
+        width: 200px;
+        padding: 20px;
+        margin-left: auto;
+        margin-right: auto;
+        filter: drop-shadow(-4px 4px 4px rgba(0, 0, 0, 0.75));
+        cursor: pointer;
+    }
+    label {
+        width: 200px;
+        text-transform: uppercase;
+        text-align: center;
+        margin-top: 10px;
+        cursor: pointer;
+        font-weight:bold
+    }
+    .circle-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: none;
+        background-color: white;
+        border-radius: 50%;
+        width: 60px;
+        height: 60px;
+        filter: drop-shadow(-4px 4px 4px rgba(0, 0, 0, 0.75));
+        cursor: pointer;
+    }
+    img {
+        width: 36px;
+        height: 30px;
+        display: block;
+    }
 </style>
